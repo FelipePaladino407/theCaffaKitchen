b19c28defca1f5e95a1a0de320986d6de1c1dba0
ManuelNey <mnet.cabrera@correo.ucu.edu.uy>
2025-06-10 12:20:19 -0300

    Cocinero

--- Proyecto/src/main/java/Clases/Cocinero.java
+++ Proyecto/src/main/java/Clases/Cocinero.java
@@ -1,4 +1,44 @@
 package Clases;
 
+import java.util.List;
+
 public class Cocinero {
+    private String nombre;
+    private Boolean ocupado;
+    private Pedido pedidoActual;
+
+    public Cocinero(String nombre, Boolean ocupado) {
+        this.nombre = nombre;
+        this.ocupado = ocupado;
+    }
+
+    public Boolean estaOcupado() {
+        return ocupado;
+    }
+
+    public void ocupar() {
+        ocupado = true;
+    }
+
+    public void liberar(){
+        ocupado = false;
+    }
+
+    public void cocinar()
+    {
+        List<Herramienta> herramientas = pedidoActual.herramientas;
+        for (Herramienta h : herramientas)
+        {
+            //estacion.adquirir()
+            //Muestro como cocino()
+            //Pasa x tiempo
+            //Entrego la comida (animación)
+            //estacion.liberar()
+        }
+    }
+    public void asignarPedidos(Pedido pedido)
+    {
+        pedidoActual = pedido;
+        //usar threat de cocinar.start()
+    }
 }
