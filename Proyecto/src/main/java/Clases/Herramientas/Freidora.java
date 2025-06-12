ba184fd315d68ca91301e6a4b102bf9339f71322
ManuelNey <mnet.cabrera@correo.ucu.edu.uy>
2025-06-12 13:20:49 -0300

    rAMA UWU

--- Proyecto/src/main/java/Clases/Herramientas/Freidora.java
+++ Proyecto/src/main/java/Clases/Herramientas/Freidora.java
@@ -4,4 +4,13 @@ public class Freidora extends Herramienta {
     public Freidora() {
         super("Freidora", 3);
     }
+    @Override
+    public void dibujarProceso() throws InterruptedException {
+        String[] frames = {"[=   ]", "[==  ]", "[=== ]", "[ ===]", "[  ==]", "[   =]"};
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\r" + nombre + " cocinando " + frames[i % frames.length]);
+            Thread.sleep(300);
+        }
+        System.out.println("\r" + nombre + " listo!  ");
+    }
 }
--- Proyecto/src/main/java/Clases/Herramientas/Horno.java
+++ Proyecto/src/main/java/Clases/Herramientas/Horno.java
@@ -2,6 +2,17 @@ package Clases.Herramientas;
 
 public class Horno extends Herramienta {
     public Horno() {
-        super("Horno", 2); // Horno: capacidad 2. Osea hay dos hornetes.
+        super("Horno", 2);
+        // Horno: capacidad 2. Osea hay dos hornetes.
+    }
+
+    @Override
+    public void dibujarProceso() throws InterruptedException {
+        String[] frames = {"[=   ]", "[==  ]", "[=== ]", "[ ===]", "[  ==]", "[   =]"};
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\r" + nombre + " cocinando " + frames[i % frames.length]);
+            Thread.sleep(300);
+        }
+        System.out.println("\r" + nombre + " listo!  ");
     }
 }
